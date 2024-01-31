b48673125a603e851daa14fceb3c465ef957c906
KazuDon-0407 <56216176+KazuDon-0407@users.noreply.github.com>
2024-01-31 20:07:27 +0900

    Update perfect_count.js

--- perfect_count.js
+++ perfect_count.js
@@ -1,25 +1,28 @@
-const href = location.href;
-const match = href.match("https://mypage.groovecoaster.jp/sp/#/mc/");
+var href = location.href;
+var match = href.match("https://mypage.groovecoaster.jp/sp/#/mc/");
 
 if (match === null) {
     alert("リザルト画面で実行してください");
 } else {
-    const music_id = href.replace(/[^0-9]/g, '');
+    var music_id = href.replace(/\D/g, '');
 
     fetch(`https://mypage.groovecoaster.jp/sp/json/music_detail.php?music_id=${music_id}`)
         .then(response => response.json())
         .then(data => {
-            let text = `あなたの${data.music_detail.music_title}のperfect回数`;
-            const score = [data.music_detail.simple_result_data, data.music_detail.normal_result_data, data.music_detail.hard_result_data, data.music_detail.extra_result_data];
-            const diff = ["simple:", "normal:", "hard:", "extra:"];
+            var text = `あなたの${data.music_detail.music_title}のperfect回数`;
+            var score = [data.music_detail.simple_result_data, data.music_detail.normal_result_data, data.music_detail.hard_result_data, data.music_detail.extra_result_data];
+            var diff = ["simple:", "normal:", "hard:", "extra:"];
     
-            const num = data.music_detail.ex_flag ? diff.length : diff.length - 1;
+            var num = data.music_detail.ex_flag ? diff.length : diff.length - 1;
 
-            for (let idx = 0; idx < num; idx++) {
-                const perfect_num = score[idx] ? `${score[idx].perfect}回` : "未プレイ";
+            for (var idx = 0; idx < num; idx++) {
+                var perfect_num = score[idx] ? `${score[idx].perfect}回` : "未プレイ";
                 text += `\n${diff[idx]}${perfect_num}`;
             }
             alert(text);
         })
-        .catch(error => console.error('Fetch Error:', error));
+        .catch(error => {
+            console.error('Fetch Error:', error);
+            alert('データの取得中にエラーが発生しました');
+        });
 }
